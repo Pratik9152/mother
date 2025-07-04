698d981bf8104f7fe58a1902824452c6af07f5f2
Pratik9152 <jfkbfjf71@gmail.com>
2025-07-04 09:04:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ button:hover {
 }
 </style>""", unsafe_allow_html=True)
 
----------- LOAD ANIMATION ----------
+#---------- LOAD ANIMATION ----------
 
 def load_lottie_url(url): r = requests.get(url) if r.status_code != 200: return None return r.json()
 
